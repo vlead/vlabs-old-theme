1b0da6ad4d35b8775efc6214d41064e73fe2eb37
Balamma Boya <balamma38@gmail.com>
2018-04-19 13:03:07 +0530

    added references and syllabus and fixed styles

--- src/search_feature/disciplines/js/app-config.js
+++ src/search_feature/disciplines/js/app-config.js
@@ -1,7 +1,7 @@
 var ldsUrl = "https://lds.vlabs.ac.in";
 var disResPath = ldsUrl + "/disciplines";
 var labImagesUrl = "https://s3-us-west-1.amazonaws.com/ld-service/images/new-lab-images/";
-var labsByDiscUrl = ldsUrl + "/labs?cached=1&consumer=lp&discipline_name=";
+var labsByDiscUrl = ldsUrl + "/labs?discipline_name=";
 //this variable can be either "openedx", "test-openedx", "cloud" or "institute"
 var openEdxHosted = "openedx"; //this can be either "openedx" or "test-openedx" etc.
 var cloudHosted = "cloud";  //this can be either "cloud" or staging etc.
